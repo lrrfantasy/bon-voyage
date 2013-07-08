f1a752b2e93568d1d38482571cd000413a88ccc4
Ruoran Liu <rrliu@thoughtworks.com>
2013-07-08 17:42:08 +0800

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BonVoyage::Application.config.secret_token = 'bd3fd4db8b0ea51bc937e2be651e8342bd792e97d1e78961ad8fd4487bdd7a81516f2bebb8671f5ed9d2477d1e416b684775a2ec4296605b884a0b4f6b1e81b1'
