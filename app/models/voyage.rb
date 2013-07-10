81b2dbb2dea9dd54df0d0c8651838d1f50c6f0bf
Ruoran Liu <rrliu@thoughtworks.com>
2013-07-10 16:45:19 +0800

    Format: single quote and inline variable reference

--- app/models/voyage.rb
+++ app/models/voyage.rb
@@ -2,50 +2,50 @@
 require 'xmlsimple'
 
 class Voyage
-  REGISTER = "注册"
-  LEARN_SKILL = "学习技能"
+  REGISTER = '注册'
+  LEARN_SKILL = '学习技能'
 
   def self.response_xml message
     content = get_content message[:from], message[:content]
     body_obj = {"ToUserName" => message[:from],
                 "FromUserName" => message[:to],
                 "CreateTime" => DateTime.now.to_i.to_s,
-                "MsgType" => "text",
+                "MsgType" => 'text',
                 "Content" => content,
-                "MsgId" => "1234567890123459"
+                "MsgId" => '1234567890123459'
     }
-    body_obj.to_xml root: "xml"
+    body_obj.to_xml root: 'xml'
   end
 
   def self.get_content user_id, content
-    message = ""
+    message = ''
     user = safe_find_user(user_id)
     if safe_equal?(user.position, REGISTER)
       user.new_user content
     elsif safe_equal?(user.position, LEARN_SKILL)
-      message += (user.learn_skill? content) ? "你成功学会了技能#{content}" : "学习技能失败\n技能不存在或者你已经学会此技能"
+      message += (user.learn_skill? content) ? "你成功学会了技能：#{content}" : "学习技能失败\n技能不存在或者你已经学会此技能"
     end
 
     if user.name.nil?
-      message += "你是新来的？取个名字吧。"
+      message += '你是新来的？取个名字吧。'
       user.save_value :position, REGISTER
       return message
     end
 
-    if content == "状态"
-      message = "姓名：" + user.name + "\n" +
-          "等级：" + user.level.to_s
+    if content == '状态'
+      message = "姓名：#{user.name}\n" +
+          "等级：#{user.level.to_s}"
     end
 
-    if content == "技能"
+    if content == '技能'
       message += "拥有的技能：\n"
       personal_skills = user.get_skills
       personal_skills.each { |skill|
-        message += "#{skill.name}：Lv#{skill.level}"
+        message += "#{skill.name}：Lv#{skill.level}\n"
       }
     end
 
-    if content == "学习技能"
+    if content == '学习技能'
       message += "请选择你要学习的技能\n"
       Skill.all.each { |skill|
         message += "#{skill.name}\n"
@@ -53,8 +53,8 @@ class Voyage
       user.save_value :position, LEARN_SKILL
     end
 
-    if message == ""
-      message = "你好，" + user.name
+    if message == ''
+      message = "你好，#{user.name}"
     end
     message
   end
@@ -69,20 +69,20 @@ class Voyage
     parsed_xml = XmlSimple.xml_in(xml, "ForceArray" => false)
     if parsed_xml["MsgType"] == "event"
       obj = {
-          to: parsed_xml["ToUserName"],
-          from: parsed_xml["FromUserName"],
-          event: parsed_xml["Event"].strip,
-          event_key: parsed_xml["EventKey"],
-          type: parsed_xml["MsgType"],
-          created_at: parsed_xml["CreateTime"]
+          to: parsed_xml['ToUserName'],
+          from: parsed_xml['FromUserName'],
+          event: parsed_xml['Event'].strip,
+          event_key: parsed_xml['EventKey'],
+          type: parsed_xml['MsgType'],
+          created_at: parsed_xml['CreateTime']
       }
     else
       obj = {
-          to: parsed_xml["ToUserName"],
-          from: parsed_xml["FromUserName"],
-          content: parsed_xml["Content"].strip,
-          type: parsed_xml["MsgType"],
-          created_at: parsed_xml["CreateTime"]
+          to: parsed_xml['ToUserName'],
+          from: parsed_xml['FromUserName'],
+          content: parsed_xml['Content'].strip,
+          type: parsed_xml['MsgType'],
+          created_at: parsed_xml['CreateTime']
       }
     end
     obj
