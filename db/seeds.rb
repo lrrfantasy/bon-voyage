51e2a12e61eed09b2f909a0e640b1b42b2355805
Ruoran Liu <rrliu@thoughtworks.com>
2013-07-19 23:07:33 +0800

    Add cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,6 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Skill.create(name: 'Accounting', max_level: 10, description: 'Bargaining')
-City.create([
-                {name: '成都', latitude: 30.661079, longitude: 104.063283},
-                {name: '长安', latitude: 34.309413, longitude: 108.949270},
-                {name: '泉州', latitude: 26.051540, longitude: 119.304829}
-            ])
 Product.create([
                 {name: '蜀锦', category: '纺织品'},
                 {name: '大米', category: '食品'},
@@ -20,7 +15,7 @@ Product.create([
                ])
 
 %w{
-  city_product
+  city city_product
 }.each do |part|
   require File.expand_path(File.dirname(__FILE__))+"/seeds/#{part}.rb"
 end
---
+++ db/seeds/city.rb
@@ -0,0 +1,25 @@
+#encoding: utf-8
+City.create([
+                {name: '成都', latitude: 30.661079, longitude: 104.063283},
+                {name: '长安', latitude: 34.309413, longitude: 108.949270},
+                {name: '泉州', latitude: 26.051540, longitude: 119.304829},
+                {name: '扬州', latitude: 32.394210, longitude: 119.412966},
+                {name: '苏州', latitude: 31.298886, longitude: 120.585316},
+                {name: '太原', latitude: 37.870590, longitude: 112.548879},
+                {name: '长沙', latitude: 28.228209, longitude: 112.938814},
+                {name: '涪陵', latitude: 29.703113, longitude: 107.389298},
+                {name: '幽州', latitude: 39.904030, longitude: 116.407526},
+                {name: '杭州', latitude: 30.274085, longitude: 120.155070},
+                {name: '洛阳', latitude: 34.618452, longitude: 112.454290},
+                {name: '灵武', latitude: 38.102655, longitude: 106.340054},
+                {name: '雁门', latitude: 39.066917, longitude: 112.960282},
+                {name: '江陵', latitude: 30.402110, longitude: 112.900079},
+                {name: '夷陵', latitude: 30.691967, longitude: 111.286471},
+                {name: '襄阳', latitude: 32.008986, longitude: 112.122415},
+                {name: '天水', latitude: 34.580864, longitude: 105.724947},
+                {name: '敦煌', latitude: 40.142130, longitude: 94.661881},
+                {name: '江夏', latitude: 30.593099, longitude: 114.305393},
+                {name: '鄱阳', latitude: 29.011699, longitude: 116.699746},
+                {name: '南海', latitude: 23.129163, longitude: 113.264435},
+                {name: '琼山', latitude: 20.030793, longitude: 110.328859}
+            ])
