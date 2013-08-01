84e840d88c78e2bcd9129f310f0b9efabb7e69af
Ruoran Liu <rrliu@thoughtworks.com>
2013-08-01 18:04:56 +0800

    Balance: change Grocery Merchant speciality from alcohol to fruit

--- db/seeds/speciality.rb
+++ db/seeds/speciality.rb
@@ -8,7 +8,7 @@ grocery_merchant = Profession.where(:name => '杂货商').first
 accounting = Skill.where(:name => '会计').first
 food_trading = Skill.where(:name => '食品贸易').first
 animal_trading = Skill.where(:name => '家畜贸易').first
-alcohol_trading = Skill.where(:name => '酒类贸易').first
+fruit_trading = Skill.where(:name => '水果贸易').first
 textile_trading = Skill.where(:name => '纺织品贸易').first
 medicine_trading = Skill.where(:name => '药材贸易').first
 grocery_trading = Skill.where(:name => '杂货贸易').first
@@ -23,7 +23,7 @@ textile_merchant.specialities.create(skill_id: accounting.id)
 textile_merchant.specialities.create(skill_id: textile_trading.id)
 
 medicine_merchant.specialities.create(skill_id: accounting.id)
-medicine_merchant.specialities.create(skill_id: alcohol_trading.id)
+medicine_merchant.specialities.create(skill_id: fruit_trading.id)
 medicine_merchant.specialities.create(skill_id: medicine_trading.id)
 
 grocery_merchant.specialities.create(skill_id: accounting.id)
